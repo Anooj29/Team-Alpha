799860724cb3b513af3cbc485f7edc286b989ea8
Parimal Ahire <parimalahire18@gmail.com>
2024-09-03 09:18:13 +0530

    screen

---
+++ screen.py
@@ -0,0 +1,36 @@
+from manim import *
+
+class FourScreensFromDot(Scene):
+    config.pixel_height = 1080
+    config.pixel_width = 1920
+    config.frame_height = 18.0
+    config.frame_width = 32.0
+    
+    def construct(self):
+        
+        positions = [
+            LEFT * 4 + UP * 2,       
+            RIGHT * 4 + UP * 2,      
+            LEFT * 4 + DOWN * 2,     
+            RIGHT * 4 + DOWN * 2    
+        ]
+
+        
+        for pos in positions:
+            
+            dot = Dot(point=ORIGIN, color=WHITE)
+            
+            
+            screen = Rectangle(width=36, height=21, color=WHITE)
+            screen.scale(0.1)
+            screen.move_to(dot.get_center())
+
+            
+            self.play(Create(dot))
+            
+            
+            self.play(Transform(dot, screen))
+            
+            
+            self.play(screen.animate.scale(1.0).move_to(pos),FadeOut(dot))
+        self.wait(2)
